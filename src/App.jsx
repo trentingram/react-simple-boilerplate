48fc2242703bcc5b63d72de03bc6bb204725fc0b
trentingram <tingram@homeaway.com>
2019-01-27 19:40:37 +0000

    adds initial HTML to App, and loads ChatBar, Message, and MessageList files.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,25 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <main className="messages">
+          <div className="message">
+            <span className="message-username">Anonymous1</span>
+            <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          </div>
+          <div className="message system">
+            Anonymous1 changed their name to nomnom.
+          </div>
+        </main>
+        <footer className="chatbar">
+          <input className="chatbar-username" placeholder="Your Name (Optional)" />
+          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        </footer>
+
+      </div>
     );
   }
 }
