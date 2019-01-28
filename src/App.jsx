1dd46db342d3a366a9b59f7dcd1c81b0bc0c422b
trentingram <tingram@homeaway.com>
2019-01-28 02:03:02 +0000

    fixes bug w/ randomNumber generator, function had no return.

--- src/App.jsx
+++ src/App.jsx
@@ -65,13 +65,17 @@ class App extends Component {
         }
       ]
       }
+      
+    randomNumber() {
+      return Math.floor((Math.random() * 100) + 1);
+    }
 
     componentDidMount() {
       console.log("componentDidMount <App />");
       setTimeout(() => {
         console.log("Simulating incoming message");
         // Add a new message to the list of messages in the data store
-        const newMessage = {id: randomNumber(), username: "Michelle", content: "Hello there!"};
+        const newMessage = {id: this.randomNumber(), username: "Michelle", content: "Hello there!"};
         const messages = this.state.messages.concat(newMessage)
         // Update the state of the app component.
         // Calling setState will trigger a call to render() in App and all child components.
@@ -80,14 +84,11 @@ class App extends Component {
     }
     
     addMessage = (text) => {
-      function randomNumber() {
-        Math.floor((Math.random() * 100) + 1);
-      }
 
       const newMessage = {
         username: this.state.currentUser.name, 
         content: text,
-        id: randomNumber()
+        id: this.randomNumber()
       };
       const messages = this.state.messages.concat(newMessage)
       this.setState({messages: messages})
