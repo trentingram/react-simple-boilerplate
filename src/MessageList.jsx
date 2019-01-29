18c1b267e2272e5045fa2e754c3b51cb4ce59410
trentingram <tingram@homeaway.com>
2019-01-29 21:34:22 +0000

    adds conditional for return Notice or Message components.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import Notification from './Notification.jsx';
 
 const MessageList = ({messages}) => {
   
-    const allMessages = messages.map((info, index) => {
-      
-      return (<Message key={info.id} messageInfo={info}/>)
+    const allMessages = messages.map((info) => {
+
+      console.log(info.type)
+      if(info.type === 'incomingMessage') {
+        return  (<Message key={info.id} messageInfo={info} />)
+      } else
+      if(info.type === 'incomingNotice') {
+        return (<Notification key={info.id} text={info.content}/>)
+      } 
     })
     return (
       <main className="messages">
