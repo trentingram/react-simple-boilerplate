b333fcf20dd2b2ef818346462b7f6fa17f4ce19c
trentingram <tingram@homeaway.com>
2019-01-29 20:14:21 +0000

    creates Notification component.

---
+++ src/Notification.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const Notification = () => {
+
+    return (
+        <div className="message system">
+          Anonymrous1 cddhanged their name to nomnom.
+        </div>
+    )
+  }
+
+  export default Notification;
