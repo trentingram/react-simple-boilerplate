3431b33aac5467fcb3c8e76a7e8d6605141d94ab
trentingram <tingram@homeaway.com>
2019-01-29 21:33:39 +0000

    moves Notice HTML to separate component.

--- src/Message.jsx
+++ src/Message.jsx
@@ -8,9 +8,6 @@ const Message = ({messageInfo}) => {
           <span className="message-username">{messageInfo.username}</span>
           <span className="message-content">{messageInfo.content}</span>
         </div>
-        <div className="message system">
-          Anonymrous1 cddhanged their name to nomnom.
-        </div>
       </li>
     )
   }
